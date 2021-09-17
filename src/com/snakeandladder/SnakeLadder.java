28c6d5867b8b2442dc61b5dffb4e9ba380098aa1
unknown <rajputnikita5666@gmail.com>
2021-09-17 21:14:42 +0530

    Adding Welcome Message

---
+++ src/com/snakeandladder/SnakeLadder.java
@@ -0,0 +1,14 @@
+package com.snakeandladder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		// UC-1-Snake and ladder game played with single player start with zero position
+		
+		int position=0;
+		System.out.println("Welcome To snake and ladder program");
+		System.out.println("You are at start position: "+""+position);
+
+	}
+
+}
